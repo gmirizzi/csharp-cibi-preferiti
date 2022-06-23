a2dd4dd4ad097fb3ed16e915521bbffaa44199e5
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-23 14:56:17 +0200

    Classifica e cibo top

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿string[] cibiPrefe = {"Pizza", "Patata", "Panzerotto", "Pasta", "Carne", "Pesce"};
+﻿string[] cibiPrefe = { "Pizza", "Patata", "Panzerotto", "Pasta", "Carne", "Pesce", "Parmigiana" };
 Console.WriteLine($"La lunghezza della classifica è pari a {cibiPrefe.Length}");
+Console.WriteLine("Classifica dei cibi preferiti:");
+for (int i = 0; i < cibiPrefe.Length; i++)
+{
+    Console.WriteLine($"{i+1}. {cibiPrefe[i]}");
+}
+Console.WriteLine("Cibo top: " + cibiPrefe[0]);
 
 
